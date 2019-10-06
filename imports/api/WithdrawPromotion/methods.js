660021163bcf61a98a663f42787775c39a2ee92e
James Oshomah <Jamiebones2000@yahoo.co.uk>
2019-10-06 06:58:53 +0100

    fixed staff promotion withdrawal reuest

--- imports/api/WithdrawPromotion/methods.js
+++ imports/api/WithdrawPromotion/methods.js
@@ -6,6 +6,9 @@ import {
 } from "./WithdrawPromotionClass";
 import { StaffMember } from "../../api/StaffMember/StaffMemberClass";
 import { PromotedStaff } from "../../api/PromotedStaff/PromotedStaffClass";
+import Notify from "../../modules/server/classes/notification";
+import Documents from "../../api/Documents/Documents";
+import { _ } from "meteor/underscore";
 
 Meteor.methods({
   "withdrawpromotion.makewithrawalrequest": function WithdrawPromotionmethod(
@@ -13,7 +16,16 @@ Meteor.methods({
   ) {
     check(withdrawObject, Object);
     const withdrawRequest = new WithdrawPromotion(withdrawObject);
-    return withdrawRequest.save();
+    withdrawRequest.save();
+    //add the notification here for the Registrar to see
+
+    const newNotification = new Notify();
+    newNotification.sendNotification({
+      accountType: "Registrar",
+      from: "Schedule Officers",
+      message:
+        "Promotion letter withdrawal: Please check and approve the list of promotions that was withdrawn. Thank you"
+    });
   },
   "withdrawpromotion.deletewithrawalrequest": function WithdrawPromotionmethod(
     id
@@ -48,6 +60,16 @@ Meteor.methods({
     staff.salaryStructure = returnToSalaryStructure;
     staff.dateOfLastPromotion = returnPromotionDate;
     staff.save();
+
+    //delete the promotion letter here
+    const staffMistakenPromoted = PromotedStaff.findOne(promotedId);
+    const reference = staffMistakenPromoted.promotionletterRef;
+    //find the document so that we can remove it.
+    Documents.findOne({
+      "meta.staffId": staffId,
+      "meta.reference": reference
+    }).remove();
+
     //remove the promotion staff from the promoted staff table
     return PromotedStaff.remove(promotedId);
   }
